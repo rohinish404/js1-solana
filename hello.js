0006cbf0cee3dbce05c884b86765e89ff0524108
Rohinish <singhrohinish09@gmail.com>
2022-11-02 05:38:44 +0000

    Update 3 files

--- hello.js
+++ hello.js
@@ -1,1 +1,16 @@
 
+const isPalindrome = (p)=>{
+    var str="";
+    for(let i = p.length-1;i>=0;i--){
+        str+=p[i]
+    }
+    return str;
+}
+
+const prompt = require("prompt-sync")();
+var p = prompt("enter the string: ")
+if(p===isPalindrome(p)){
+    console.log("the string entered is palindrome")
+}else{
+    console.log("the string entered is not a palindrome")
+}
